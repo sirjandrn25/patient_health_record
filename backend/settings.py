0142494ea3f64ef15e9685cdbd978cb1c688ae4b
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-22 11:37:53 +0545

    debug false

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["phr21.herokuapp.com","localhost"]
 
