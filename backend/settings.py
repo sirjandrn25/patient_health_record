ac487ebe9db11bfaf91337c7577c50d500bb27c8
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-22 08:57:32 +0545

    static fix issue

--- backend/settings.py
+++ backend/settings.py
@@ -142,8 +142,9 @@ if DEBUG:
   STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-  STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
