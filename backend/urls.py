9e0163ce401e0351c0ff77b06607be81f358975d
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-22 09:14:36 +0545

    fix serve

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from django.views.static import 
+from django.views.static import serve
 
 schema_view = get_schema_view(
    openapi.Info(
