4f5cb447cba546145f30b3ffbf4df43504714755
drnsirjan25 <sirjantmg99@gmail.com>
2021-12-22 08:47:39 +0545

    fix

--- commands.py
+++ commands.py
@@ -1,2 +1,2 @@
-git remote add origin https://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@github.com/sirjandrn25/patient_health_record.git
+git remote add origin https://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@github.com/sirjandrn25/patient_health_record.git
 
